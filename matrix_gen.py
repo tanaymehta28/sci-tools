a2c7e828b0a14433b04e89bf0ee71ea08ab254a3
Tanay Mehta <tanaymehta28@gmail.com>
2018-12-30 18:47:45 +0530

    Create matrix_gen.py

---
+++ matrix_gen.py
@@ -0,0 +1,25 @@
+import random
+class oneD:
+    def __init__(self,n):
+        self.n = n
+        self.array = array
+        self.temp_03cxfc = temp_03cxfc
+    def zeroes(n):
+        array=[0 for temp_03cxfc in range(n)]
+        return array
+    def rand(n):
+        array = [random.random() for temp_03xfc in range(n)]
+        return array
+class twoD:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b 
+        self.array = array
+        self.temp_03cfg = temp_03cfg
+        self.temp_03cfh = temp_03cfh
+    def zeroes(a,b):
+        array = [[0 for temp_03cfg in range(a)] for temp_03cfh in range(b)]
+        return array
+    def rand(a,b):
+        array = [[random.random() for temp_03cfg in range(a)] for temp_03cfh in range(b)]
+        return arrays
